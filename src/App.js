0ddccb265cd98fcafe05c91814a050e8dce99c7e
Tyson Hughes <tyson.hughes@live.com>
2023-08-21 15:20:23 -0600

    got todo started, with button, modal, and backdrop, completely static

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import Backdrop from "./components/Backdrop";
+import Modal from "./components/Modal";
+import Todo from "./components/Todo";
+
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <header className="bg-gray-400 p-4">
+        <h1 className="text-white text-2xl font-semibold">My Todo List</h1>
+      </header>
+      <div className="flex flex-col mt-3">
+        <Todo title={"clean house"} />
+        <Todo title={"learn react"} />
+        <Todo title={"do next.js"} />
+        <Modal />
+        <Backdrop />
+      </div>
+    </div>
+  );
 }
 
 export default App;
